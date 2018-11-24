43c06ae2b83defe3c4d4e178696179c79e194b17
Patricia LG <patri.lopg5@gmail.com>
2018-11-24 17:52:25 +0100

    Comments, Map, KioskPlaces, EditProfile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const mapsRoutes = require("./maps");
+const kioskPlacesRoutes = require("./kioskPlaces");
+const commentsRoutes= require("./comments");
+const editProfileRoutes = require("./editProfile");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
+
+router.use("/maps", mapsRoutes);
+router.use("/kioskPlaces", kioskPlacesRoutes);
+router.use("/comments", commentsRoutes);
+router.use("/editProfile", editProfileRoutes);
+
 
 module.exports = router;
